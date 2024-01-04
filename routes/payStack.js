5fa6a70edf8e3fc45559995c934cdfedd4e99f97
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-01-04 20:24:31 +0300

    Cleaner route handler

--- routes/payStack.js
+++ routes/payStack.js
@@ -1,52 +1,10 @@
 const express = require('express');
-const https = require('https');
-const paystackLiveSecret = process.env['PAYSTACK_LIVE_SECRET'];
+const { requestPayment, paymentCallback } = require('../controllers/payStack');
 
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    const { email, amount } = req.query;
-    const params = JSON.stringify({
-        email, amount, currency: 'KES'
-    })
+router.get('/', requestPayment);
 
-    const options = {
-        hostname: 'api.paystack.co',
-        port: 443,
-        path: '/transaction/initialize',
-        method: 'POST',
-        headers: {
-            Authorization: 'Bearer ' + paystackLiveSecret,
-            'Content-Type': 'application/json'
-        }
-    }
-
-    const request = https.request(options, response => {
-        let data = ''
-
-        response.on('data', (chunk) => {
-            data += chunk
-        });
-
-        response.on('end', () => {
-            console.log(JSON.parse(data));
-            res.status(200).json(JSON.parse(data));
-        })
-    }).on('error', error => {
-        console.error(error)
-        res.status(500).json({ message: 'Something went wrong', status: 500 });
-    })
-
-    request.write(params)
-    request.end()
-})
-
-router.post('/', (req, res)=>{
-    console.log(req.body);
-
-    res.status(200).json({
-        message: 'Post request received successfully'
-    })
-});
+router.post('/', paymentCallback);
 
 module.exports = router;
